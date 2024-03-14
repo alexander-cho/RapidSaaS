73837aa67751bc4774f09abcf18f960902cd1a24
Alex Cho <alex.jhcho@gmail.com>
2024-03-13 23:30:53 -0700

    feat: generic view for submissions

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'feed/home.html', {})
+# def home(request):
+#     return render(request, 'feed/home.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'feed/home.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'feed/post_detail.html'
