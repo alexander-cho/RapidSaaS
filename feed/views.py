ee70dad97f9cdfe80fd681adf56193c863d190b0
Alex Cho <alex.jhcho@gmail.com>
2024-03-15 19:42:32 -0700

    feat: add form and model fields and styling

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -19,6 +20,7 @@ class PostDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'feed/add_post.html'
     # fields = ['title', 'body']
-    fields = '__all__'
+    # fields = '__all__'
