d63f6cbbf4abe94d456dc1ac29bc86f58751c4ee
Alex Cho <alex.jhcho@gmail.com>
2024-03-23 21:38:05 -0700

    feat: add login/logout

--- feed/views.py
+++ feed/views.py
@@ -5,6 +5,7 @@ from .forms import IdeaForm
 from django.urls import reverse_lazy
 from django.http import JsonResponse
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -56,6 +57,28 @@ def profile(request, pk):
     else:
         messages.success(request, ('Login to view this page'))
         return redirect('home')
+    
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username'] # refers to name= .... in template
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have been logged in'))
+            return redirect('home')
+        else:
+            messages.success(request, ('There was an error'))
+            return redirect('login')
+    else:
+        return render(request, 'feed/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out'))
+    return redirect('home')
 
 
 # class HomeView(ListView):
