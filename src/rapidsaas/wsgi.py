04b0c1de92c136688741d8bbb0c7621f34e7728f
Alex Cho <alex.jhcho@gmail.com>
2024-07-28 23:39:31 -0700

    feat: provision http responses

--- src/rapidsaas/wsgi.py
+++ src/rapidsaas/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rapidsaas.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rapidsaas.settings')
 
 application = get_wsgi_application()
