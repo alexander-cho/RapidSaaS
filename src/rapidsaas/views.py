ca38c8d5682f72d649c3cd85ecdc23303dfd70a3
Alex Cho <alex.jhcho@gmail.com>
2024-07-29 23:02:30 -0700

    feat: test pythonic html response

--- src/rapidsaas/views.py
+++ src/rapidsaas/views.py
@@ -1,5 +1,17 @@
+import pathlib
 from django.http import HttpResponse
 
+this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Hello there!</h1>')
+    # path = this_dir / 'home.html'
+    # html_ = path.read_text()
+    html_ = """
+            <!DOCTYPE html>
+            <html>
+                <body>
+                    <h1>In line html</h1>
+                </body>
+            </html>
+            """ 
+    return HttpResponse(html_)
