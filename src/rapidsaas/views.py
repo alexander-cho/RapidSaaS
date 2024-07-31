1cd147c88e05e1639617905ab5ce7e2b510de5ca
Alex Cho <alex.jhcho@gmail.com>
2024-07-30 19:03:09 -0700

    feat: start templates

--- src/rapidsaas/views.py
+++ src/rapidsaas/views.py
@@ -1,17 +1,33 @@
 import pathlib
 from django.http import HttpResponse
+from django.shortcuts import render
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
     # path = this_dir / 'home.html'
     # html_ = path.read_text()
+    my_title = 'RapidSaaS Prototype'
+    context = {
+        'my_title': my_title
+    }
+    html_template = 'home.html'
+    return render(request, html_template, context)
+
+
+def old_home_page_view(request, *args, **kwargs):
+    # path = this_dir / 'home.html'
+    # html_ = path.read_text()
+    my_title = 'RapidSaaS Prototype'
+    context = {
+        'my_title': my_title
+    }
     html_ = """
             <!DOCTYPE html>
             <html>
                 <body>
-                    <h1>In line html</h1>
+                    <h1>{my_title}</h1>
                 </body>
             </html>
-            """ 
+            """.format(**context)
     return HttpResponse(html_)
