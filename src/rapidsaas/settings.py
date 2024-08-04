2173418bf395c6fbb586047922b86f3ab2da3083
Alex Cho <alex.jhcho@gmail.com>
2024-08-04 09:29:39 -0700

    chore: add python-decouple and configure .env for Django settings

--- src/rapidsaas/settings.py
+++ src/rapidsaas/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ot^8v0)w0287c2if-wh(e*-m87kvnlhtcr2+w_y--l8y7ksx9z'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DJANGO_DEBUG', cast=bool)
+
 
 ALLOWED_HOSTS = [
     '.railway.app'
